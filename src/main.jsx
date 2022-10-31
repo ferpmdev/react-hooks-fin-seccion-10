6fe4a71e3aa1a96d587d6f3ab0cf5df21121c038
ferpmdev <ferpm.dev@gmail.com>
2022-10-31 08:06:16 -0300

    termine useReducer y empiezo Context en clase 155

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 // import { HooksApp } from './HooksApp';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
-// import { SimpleForm } from './02-useEffect/SimpleForm';
+// import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './04-useRef/FocusScreen';
@@ -15,12 +14,12 @@ import ReactDOM from 'react-dom/client';
 // import { CallbackHook } from './06-memos/CallbackHook';
 // import { Padre } from './07-tarea-memo/Padre';
 // import './08-useReducer/intro-reducer';
-import { TodoApp } from './08-useReducer/TodoApp';
+import { TodoApp } from "./08-useReducer/TodoApp";
 
-import './index.css';
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <TodoApp />
+  <TodoApp />
   // </React.StrictMode>
-)
+);
